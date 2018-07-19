668012b3bbc10ce98a2f734dead422ff5a2dbb22
SERDAR DZHEMILYEV <serdardzhemilyev@std.sehir.edu.tr>
2018-07-20 00:12:12 +0300

    #6 Covered: textures, texture units, more on uniforms

--- Application.cpp
+++ Application.cpp
@@ -2,6 +2,8 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include "Shader.h"
+#include "stb_image.h"
+#include <string>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -48,12 +50,12 @@ int main()
 
 	Shader shader("shaders/vertex.shader", "shaders/fragment.shader");
 
-	float vertices[] = {
-		// positions          // colors
-		 0.5f,  0.5f,  0.0f,  1.0f, 0.0f, 0.0f,   // top right		#0
-		 0.5f, -0.5f,  0.0f,  0.0f, 1.0f, 0.0f,   // bottom right	#1
-		-0.5f, -0.5f,  0.0f,  0.0f, 0.0f, 1.0f,   // bottom left	#2
-		-0.5f,  0.5f,  0.0f,  1.0f, 0.0f, 1.0f    // top left		#3
+	float vertices[] = {	
+		 // positions         // colors			   // texture coords
+		 0.5f,  0.5f,  0.0f,  1.0f, 0.0f, 0.0f,    1.0f,  1.0f,	// top right	#0
+		 0.5f, -0.5f,  0.0f,  0.0f, 1.0f, 0.0f,    1.0f,  0.0f,	// bottom right	#1
+		-0.5f, -0.5f,  0.0f,  0.0f, 0.0f, 1.0f,    0.0f,  0.0f,	// bottom left	#2
+		-0.5f,  0.5f,  0.0f,  1.0f, 1.0f, 0.0f,    0.0f,  1.0f	// top left		#3
 	};
 
 	unsigned int indices[] = {
@@ -61,13 +63,79 @@ int main()
 		1, 2, 3
 	};
 
-	//binding it with variable, initialising it
-	unsigned int VBO, VAO;
-	unsigned int EBO;
+	float texCoords[] = {
+		 1.0f,  1.0f,
+		 1.0f, -1.0f,
+		-1.0f, -1.0f,
+		-1.0f,  1.0f
+	};
+
+	unsigned int VBO, VAO, EBO, texture1, texture2;
 
 	glGenVertexArrays(1, &VAO);		// Generating vertex array object
 	glGenBuffers(1, &VBO);			// Generating vertex buffer object
 	glGenBuffers(1, &EBO);			// Generating element buffer object
+
+	glGenTextures(1, &texture1);		// Generating texture object
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	// Setting the texture wrapping/filtering options(on currently bount texture oject)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_REPEAT);
+	
+	int width, height, nrChannels;
+	std::string path = "textures/delogo.png";
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		/* Previous func args:
+		1. Texture target
+		2. Mipmap level - manual setting
+		3. Format to store the texture - image has only rgb values so rgb values are stored (!!! Change for maps)
+		4,5. Width, height - correspondingly
+		6. Should always be 0 - legacy staff
+		7,8. Format and datatype of source image - we loaded RGB values and stored as chars(bytes) (!!! Change for maps)
+		9. Actual image data (pointer)
+		*/
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << path << std::endl;
+	}
+	stbi_image_free(data);
+		
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_REPEAT);
+	path = "textures/awesomeface.png";
+	data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		/* Previous func args:
+		1. Texture target
+		2. Mipmap level - manual setting
+		3. Format to store the texture - image has only rgb values so rgb values are stored (!!! Change for maps)
+		4,5. Width, height - correspondingly
+		6. Should always be 0 - legacy staff
+		7,8. Format and datatype of source image - we loaded RGB values and stored as chars(bytes) (!!! Change for maps)
+		9. Actual image data (pointer)
+		*/
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << path << std::endl;
+	}
+	stbi_image_free(data);
+
 
 	// 1. Bind vertex array object
 	glBindVertexArray(VAO);
@@ -78,11 +146,16 @@ int main()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	// 4. Then set the vertex attributes pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+	
+	shader.use();
+	shader.setUniform1i("texture1", 0);
+	shader.setUniform1i("texture2", 1);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -91,9 +164,12 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f); /* Sets a color to clear screen with. Function is a state-setting function*/
 		glClear(GL_COLOR_BUFFER_BIT); /* Actually clears the screen. Function is a state using function. */
 
-		
-		shader.use();
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
 
+		shader.use();
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		
